a62a611cf644be139267e3e2a12c60dde984c668
ebuka-hub <albertebuka69@gmail.com>
2025-04-19 07:10:53 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+class cat:
+    def __init__(self,name,age):
+    self.name = name
+    self.age = age
+
+    def info(self):
+        print(f"I am a cat. My name is {self.name}.I am {self.age} years old")
+
+        def Make__sound(self)
+        print("Meow")
+
+
+        class dog:
+            def __init__(self , name , age):
+                self.name = name
+                self.age = age
+
+                def info(self):
+                    print(f"I am a dog. My name is {self.name}.I am {self.age} years old")
+
+                    def Make__sound(self)
+                    print("brak")
+
+
+                    
